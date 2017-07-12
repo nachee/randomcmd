ed99109b95326ebfb92de8fb3ad222b5750848d8
Ignacio Basail <basail.ignacio@gmail.com>
2017-07-12 15:20:35 +1000

    Initial Commit

---
+++ randomcmd/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  randomcmd
+//
+//  Created by Ignacio Basail on 12/07/2017.
+//  Copyright © 2017 Ignacio Basail. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
